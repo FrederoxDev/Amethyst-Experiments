96b5729eb534c9c9c2020e636720dd4799e8d88d
FrederoxDev <blakefreddie96@gmail.com>
2024-05-04 18:33:21 +0100

    Update main branch of Amethyst-Experiments, and remove accidental content inside

--- src/dllmain.h
+++ src/dllmain.h
@@ -1,11 +1,6 @@
 #pragma once
 #include <Windows.h>
-#include <amethyst/runtime/AmethystContext.h>
-#include <minecraft/src-client/common/client/input/ClientInputHandler.h>
-#include <amethyst/runtime/input/InputManager.h>
-#include <minecraft/src-client/common/client/gui/controls/ToggleComponent.h>
-#include <minecraft/src-client/common/client/gui/controls/SliderComponent.h>
-#include <minecraft/src-client/common/client/gui/controls/TextComponent.h>
+#include <amethyst/runtime/AmethystContext.hpp>
 
 #define ModFunction extern "C" __declspec(dllexport)
 
