a9a37a0da2642265e0d94ca376bb4056081a8377
FrederoxDev <blakefreddie96@gmail.com>
2024-05-07 20:17:05 +0100

    registering an item on 1.20.71.1

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -1,8 +1,28 @@
 ﻿#include "dllmain.h"
+#include <minecraft/src/common/world/item/registry/ItemRegistryManager.hpp>
 
 AmethystContext* amethyst;
 
 ModFunction void Initialize(AmethystContext* _amethyst)
 {
 	amethyst = _amethyst;
+
+	Log::Info("hello!");
+	amethyst->mEventManager.registerItems.AddListener(&RegisterItems);
+}
+
+SharedPtr<Item> testItem;
+
+void RegisterItems(ItemRegistry* registry) 
+{
+	if (registry == nullptr) {
+		Log::Info("ItemRegistry* was nullptr");
+		return;
+	}
+
+	Log::Info("Register Items");
+
+	testItem = SharedPtr(new Item("test_item", ++registry->mMaxItemID));
+
+	registry->registerItem(testItem);
 }
--- src/dllmain.h
+++ src/dllmain.h
@@ -7,3 +7,5 @@
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
     return TRUE;
 }
+
+void RegisterItems(ItemRegistry* registry);
